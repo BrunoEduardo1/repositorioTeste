7464f979a861217b9ef1caf7663a4a22c98ce897
BrunoEduardo1 <hueteste2@gmail.com>
2018-05-25 16:25:35 -0300

    Quem somos e galeria adicionados

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,22 @@ $(document).ready(function () {
 		}
 		$("#alertaC").html(a);
 	});
+
+	 $("#galeria-btn-nav, #form-btn-nav").on('click', function(e) {
+               // prevent default anchor click behavior
+                e.preventDefault();
+
+                // store hash
+                var hash = this.hash;
+
+                // animate
+                $('html, body').animate({
+                       scrollTop: $(hash).offset().top
+                }, 500, function(){
+                     // when done, add hash to url
+                    // (default click behaviour)
+                       window.location.hash = hash;
+                     });
+                }); 
 		// body...
 })
